0b080f00047a7f88686c4c5cf812ab23bbf3acfc
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-11 20:13:54 +0100

    subscription html

--- subscription.php
+++ subscription.php
@@ -1,7 +1,27 @@
 <?php include "partials/header.php"; ?>
 
-     <!-- concept -->
-     <section class="concept container">
+    <section class="subscription">
+        <article class="container">
+            <div>
+                <h2>Subscription from $15/month</h2>
+                <h4>Start a free trial now</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat magni officiis, quibusdam doloremque cupiditate perferendis! Dolore, iure totam assumenda dolor eum magni unde consequatur minima illum quos ipsum rerum qui!</p>
+                <button>Start free trial now</button>
+            </div>
+            <div>
+                <ul>
+                    <li>Play any track</li>
+                    <li>Listen offline</li>
+                    <li>No add interruptions</li>
+                    <li>Unlimited skips</li>
+                    <li>High quality</li>
+                    <li>Shuffle play</li>
+                </ul>
+            </div>
+        </article>
+    </section>
+    <!-- concept -->
+    <section class="concept container">
         <article>
             <h2>Our Concept & artists</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem nisi officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto sapiente laboriosam.
