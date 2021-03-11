4583c292978605ded328613f145eb472dc8e4c94
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-11 10:56:10 +0100

    header html

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php include "partials/header.php"; ?>
-
-    <h1 style="font-size:9rem; text-align:center">Home</h1>
+    <!-- header -->
+    <header class="mainHeader">
+        <div>
+            <h1><span>Music</span> for everyone</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi labore repellat veniam cum, dolores delectus, velit incidunt, ducimus ea consequuntur possimus excepturi obcaecati inventore cumque! Harum vero odio, quibusdam voluptatem!</p>
+            <button>Start free trial</button>
+        </div>
+        <div>
+            <img src="img/header.png" alt="">
+        </div>
+    </header>
     
 <?php include "partials/footer.php"; ?>
